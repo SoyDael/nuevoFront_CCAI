365e529cf26e3a8f19a6ea49efefe5d451ed547e
Natalia Ortiz <81204117+NataliaOrtizLopez@users.noreply.github.com>
2024-05-14 12:22:41 -0600

    casi me lo borra
    Co-Authored-By: Saul Dael Guzman Hernandez <123768353+SoyDael@users.noreply.github.com>

--- src/components/registroComponents/AlumnoInternoComponent/AlumnoInterno.jsx
+++ src/components/registroComponents/AlumnoInternoComponent/AlumnoInterno.jsx
@@ -1,31 +1,207 @@
-import React from 'react'
-import NavbarSimple from '../../navbarComponents/NavbarSimple'
-import { registroEstudiante } from '../../../api/APIS';
+import React from "react";
+import NavbarSimple from "../../navbarComponents/NavbarSimple";
+import { registroEstudiante } from "../../../api/APIS";
+import Swal from "sweetalert2";
 
 const AlumnoInterno = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const estudiante = Object.fromEntries(formData);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const estudiante = Object.fromEntries(formData);
+    try {
+      await registroEstudiante(estudiante);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Alumno Interno añadido correctamente.",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      e.target.reset();
+    } catch (error) {
+      console.error("Error al añadir alumno:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Error al añadir alumno. Por favor, inténtalo de nuevo.",
+      });
+    }
+  };
 
-        try {
-            await registroEstudiante(estudiante);
-            alert('Alumno Interno añadido correctamente');
-            e.target.reset();
-        } catch (error) {
-            console.error('Error al añadir alumno:', error);
-            alert('Error al añadir alumno. Por favor, inténtelo de nuevo más tarde.');
-        }
-    };
+  return (
+    <>
+      <NavbarSimple />
+      <br /><br /><br /><br /><br />
+      <form class="max-w-xl mx-auto">
+        <div class="grid md:grid-cols-2 md:gap-6">
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_first_name"
+              id="floating_first_name"
+              class="block py-3 px-3 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_first_name"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Matricula:
+            </label>
+          </div>
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_last_name"
+              id="floating_last_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_last_name"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Nombre(s):
+            </label>
+          </div>
+        </div>
+        <div class="grid md:grid-cols-2 md:gap-6">
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_first_name"
+              id="floating_first_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_first_name"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Apellido paterno:
+            </label>
+          </div>
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_last_name"
+              id="floating_last_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_last_name"
+              class="peer-focus: font-bold absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Apellido materno:
+            </label>
+          </div>
+        </div>
+        <div class="grid md:grid-cols-2 md:gap-6">
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_first_name"
+              id="floating_first_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_first_name"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Correo institucional:
+            </label>
+          </div>
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="floating_last_name"
+              id="floating_last_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_last_name"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Correo adicional:
+            </label>
+          </div>
+        </div>
+        <div class="grid md:grid-cols-2 md:gap-6">
+          <div class="relative z-0 w-full mb-5 group">
+            <input
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              name="floating_phone"
+              id="floating_phone"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_phone"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Telefóno: (123-456-7890):
+            </label>
+          </div>
+          <div class="relative z-0 w-full mb-5 group">
+            <select  type="text"
+              name="floating_company"
+              id="floating_company"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required>
+                
+             <option value="division"  > Selecciona una opción </option>
+             <option value="division"  > Ingenería Informática </option>
+             <option value="division"> Ingenería en Sistemas Computacionales </option>
+             <option value="division"> Ingenería Electrónica </option>
+             <option value="division"> Ingenería Mecánica </option>
+             <option value="division"> Ingenería Bioquímica </option>
+             <option value="division"> Ingenería Química </option>
+             <option value="division"> Ingenería Industrial </option>
+             <option value="division"> Ingenería Mecatrónica </option>
+             <option value="division"> Ingenería en Gestión Empresarial </option>
+             <option value="division"> Ingenería Aeronáutica </option>
+             <option value="division"> Contador Público </option>
+              </select>
+            <label
+              for="floating_company"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              División:
+            </label>
+          </div>
+        </div>
+        <button
+          type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
 
-    return (
-        <>
-            <NavbarSimple />
+export default AlumnoInterno;
+
+{
+  /** 
             <div className='bg-gray-100 flex flex-col justify-center items-center min-h-screen'>
                 <div className='lg:p-35 md:p-16 sm:20 p-8 w-full lg:w-1/2'>
                     <h1 className='text-2xl font-semibold mb-4'>Registro Alumno Interno</h1>
-                    <form action="" onSubmit={handleSubmit}>
+                    <form action="" onSubmit={handleSubmit} className='max-w-md mx-auto'>
                         <div className='mb-4'>
                             <label htmlFor="matricula" className='block text-gray-600'>Matricula: </label>
                             <input type="text" name="matricula" id="matricula" required
@@ -94,9 +270,5 @@ const AlumnoInterno = () => {
                     </div>
                 </div>
             </div>
-        </>
-
-    )
+            */
 }
-
-export default AlumnoInterno
