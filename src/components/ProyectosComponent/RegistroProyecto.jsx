994861db994ad4ebd0aaa2bbcb2ceca07cfd7400
SoyDael <guhs202011004@tese.edu.mx>
2024-05-20 22:10:27 -0600

    Update RegistroProyecto.jsx

--- src/components/ProyectosComponent/RegistroProyecto.jsx
+++ src/components/ProyectosComponent/RegistroProyecto.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
 import { registroProyecto, consultaInvestigadores } from '../../api/APIS';
 import SlideBarInvestigadores from '../SlideBar/SlideBarInvestigadores';
 
@@ -7,9 +6,6 @@ const RegistroProyecto = () => {
 
   const [Investigador, setInvestigador] = useState([])
 
-  const RedireccionarInvestigador = (correo) => {
-    navigate(`/perfilInvestigador/${correo}`);
-  }
 
   useEffect(() => {
     const fetchInvestigador = async () => {
@@ -28,16 +24,15 @@ const RegistroProyecto = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-
+    const data = Object.fromEntries(formData);
+    console.log("Datos del proyecto ", data);
     try {
-      await registroProyecto(formData);
-      console.log(formData);
-      alert('Proyecto añadido correctamente');
-
+      const response = await registroProyecto(data);
+      console.log(response);
+      alert('Proyecto registrado con éxito');
     } catch (error) {
-      console.error('Error al añadir proyecto', error);
-      alert('Error al añadir proyecto. Por favor, intentalo de nuevo')
-
+      console.error('Error al registrar proyecto:', error);
+      alert('Error al registrar proyecto. Por favor, inténtalo de nuevo.');
     }
   }
 
@@ -51,14 +46,20 @@ const RegistroProyecto = () => {
         <div className=" rounded-md p-4 relative  border shadow-2xl bg-gray-800 border-gray-700   shadow-blue-500/50  ">
           <h1 class="text-4xl font-semibold mb-4">Registro proyecto</h1>
           <form onSubmit={handleSubmit}>
-            <br />
             <div class="flex flex-col items-center justify-center space-y-6">
-              <input type="text" id="titulo_esp" name="titulo_esp" placeholder="Titulo" class="w-96 appearance-none rounded-full border-0 bg-slate-700 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
+              <input  
+              type="text" 
+              id="titulo_esp" 
+              name="titulo_esp" 
+              placeholder="Titulo" 
+              class="w-96 appearance-none rounded-full border-0 bg-slate-700 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500"
+               />
               <textarea id="descripcion" name="descripcion" placeholder="Descripción" class="w-96 appearance-none rounded-full border-0 bg-slate-700 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
               <textarea id="objetivo" name="objetivo" placeholder="Objetivo" class="w-96 appearance-none rounded-full border-0 bg-slate-700 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
 
               <select
                 className="w-96 appearance-none rounded-full border-0 bg-slate-700 p-2 px-4 focus:bg-slate-800 focus:ring-2"
+                id='coordinador_correo'
                 name="coordinador_correo"
               >
                 <option value="">Selecciona un Investigador</option>
@@ -83,19 +84,12 @@ const RegistroProyecto = () => {
               <input type="date" id="fecha_inicio" name="fecha_inicio" placeholder="Fecha de inicio" class="w-96 appearance-none rounded-full border-0 bg-slate-700 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
               <input type="date" id="fecha_fin" name="fecha_fin" placeholder="Fecha de finalización" class="w-96 appearance-none rounded-full border-0 bg-slate-700 p-2 px-4 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
 
-
-
-              <button id="showPw" class="rounded-full bg-indigo-500 p-2 px-4 text-white hover:bg-indigo-700"><span id="showHide">Añadir</span> Proyecto</button>
+              <button t id="showPw" class="rounded-full bg-indigo-500 p-2 px-4 text-white hover:bg-indigo-700"><span id="showHide">Añadir</span> Proyecto</button>
 
             </div>
           </form>
         </div>
       </section>
-
-
-
-
-
     </>
 
   )
