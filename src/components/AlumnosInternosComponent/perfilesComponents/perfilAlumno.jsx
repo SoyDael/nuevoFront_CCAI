05264e74bceb2b3abac85c0d8a7b082ca06df5e5
Natalia Ortiz <81204117+NataliaOrtizLopez@users.noreply.github.com>
2024-05-21 11:34:14 -0600

    cambio perfil alumno
    nat cambio de interfaz de alumno

--- src/components/AlumnosInternosComponent/perfilesComponents/perfilAlumno.jsx
+++ src/components/AlumnosInternosComponent/perfilesComponents/perfilAlumno.jsx
@@ -77,44 +77,54 @@ const PerfilAlumno = () => {
 
     return (
         <>
-            <SlideBarPruebaAlumn />
-            <div class="flex justify-center items-center h-screen bg-slate-400 pt-12">
-                <div class="mx-auto bg-slate-800 shadow-blue-500/100 rounded-lg overflow-hidden shadow-lg">
-                    <div class="border-b px-4 pb-6">
-                        <div class="text-center my-4">
-                            {/**    <img class="h-32 w-32 rounded-full border-4 border-white dark:border-gray-800 mx-auto my-4" src="" alt="" >{perfilEstudiante[0]?.foto}</img> */}
-                            <div class="py-2">
-                                <h3 class="font-bold text-2xl text-white dark:text-dark mb-1">Bienvenido(a) <br></br> {perfilEstudiante[0]?.nombres} {perfilEstudiante[0]?.apellido_p} {perfilEstudiante[0]?.apellido_m}</h3>
-                                <br></br>
-                                <div class="inline-flex text-gray-700 dark:text-gray-300 items-center">
-                                    <h3 class="text-2xl text-white dark:text-dark mb-1">Division: <br></br> {perfilEstudiante[0]?.division}</h3>
-                                </div>
+        <SlideBarPruebaAlumn />
+        {perfilEstudiante.length > 0 && (
+        <div class="flex justify-center items-center h-screen bg-slate-400 pt-12">
+        <div className="relative w-full group max-w-md min-w-0 mx-auto mt-6 mb-6 break-word  bg-slate-800 border shadow-2xl dark:bg-gray-800 dark:border-gray-700 md:max-w-sm rounded-xl  shadow-blue-500/50">
+                <div class="pb-6">
+                    <div class="text-center my-4">
+                        {/**    <img class="h-32 w-32 rounded-full border-4 border-white dark:border-gray-800 mx-auto my-4" src="" alt="" >{perfilEstudiante[0]?.foto}</img> */}
+                        <div class="py-2">
+                            <h3 class="font-bold text-2xl text-slate-300 dark:text-dark mb-1">Bienvenido(a) <br></br> {perfilEstudiante[0]?.nombres} {perfilEstudiante[0]?.apellido_p} {perfilEstudiante[0]?.apellido_m} </h3>
+                            <br></br>
+                            <div class="inline-flex text-slate-300 dark:text-gray-300 items-center">
+                                <h3 class="text-2xl text-slate-300 dark:text-dark mb-1">Division: <br></br> {perfilEstudiante[0]?.division}</h3>
                             </div>
-                            <div class="py-2">
-                                <div class="inline-flex text-gray-700 dark:text-gray-300 items-center">
-                                    <h3 class="text-2xl text-white dark:text-dark mb-1">Matricula:<br></br> {perfilEstudiante[0]?.matricula}</h3>
-                                </div>
+                        </div> 
+                        <div class="py-2">
+                            <div class="inline-flex text-slate-300 dark:text-gray-300 items-center">
+                                <h3 class="text-2xl text-slate-300 dark:text-dark mb-1">Matricula:<br></br> {perfilEstudiante[0]?.matricula}</h3>
                             </div>
-                            <div class="py-2 items-center">
-                                <div class="inline-flex justify-center text-gray-700 dark:text-gray-300 items-center">
-                                    <h3 class="text-2xl text-white dark:text-dark mb-1">Correo: {perfilEstudiante[0]?.correo}</h3>
-                                </div>
+                        </div>
+                        <div class="py-2 items-center">
+                            <div class="inline-flex justify-center text-gray-700 dark:text-gray-300 items-center">
+                                <h3 class="text-2xl text-slate-300 dark:text-dark mb-1">Correo: {perfilEstudiante[0]?.correo}</h3>
                             </div>
-                            <div class="py-2 items-center">
-                                <div class="inline-flex justify-center text-gray-700 dark:text-gray-300 items-center">
-                                    <h3 class="text-2xl text-white dark:text-dark mb-1">Correo Adicional: {perfilEstudiante[0]?.correo_adicional}</h3>
-                                </div>
+                        </div>
+                        <div class="py-2 items-center">
+                            <div class="inline-flex justify-center text-slate-300 dark:text-gray-300 items-center">
+                                <h3 class="text-2xl text-slate-300 dark:text-dark mb-1">Correo Adicional: {perfilEstudiante[0]?.correo_adicional}</h3>
                             </div>
-                            <div class="py-2 items-center">
-                                <div class="inline-flex justify-center text-gray-700 dark:text-gray-300 items-center">
-                                    <h3 class="text-2xl text-white dark:text-dark mb-1">Telefono:<br></br> {perfilEstudiante[0]?.telefono}</h3>
-                                </div>
+                        </div>
+                        <div class="py-2 items-center">
+                            <div class="inline-flex justify-center text-slate-300 dark:text-gray-300 items-center">
+                                <h3 class="text-2xl text-slate-300 dark:text-dark mb-1">Telefono:<br></br> {perfilEstudiante[0]?.telefono}</h3>
                             </div>
+                        </div>
+                    </div>
+                    <div className="relative h-6 overflow-hidden translate-y-6 rounded-b-xl">
+                        <div className="absolute flex -space-x-12 rounded-b-2xl">
+                            <div className="w-36 h-8 transition-colors duration-200 delay-75 transform skew-x-[35deg] bg-gray-400/90 group-hover:bg-sky-600/90 z-10"></div>
+                            <div className="w-28 h-8 transition-colors duration-200 delay-100 transform skew-x-[35deg] bg-sky-300/90 group-hover:bg-gray-500/90 z-20"></div>
+                            <div className="w-28 h-8 transition-colors duration-200 delay-150 transform skew-x-[35deg] bg-gray-200/90 group-hover:bg-sky-400/90 z-30"></div>
+                            <div className="w-28 h-8 transition-colors duration-200 delay-200 transform skew-x-[35deg] bg-sky-100/90 group-hover:bg-gray-300/90 z-40"></div>
+                            <div className="w-28 h-8 transition-colors duration-200 delay-300 transform skew-x-[35deg] bg-gray-50/90 group-hover:bg-sky-200/90 z-50"></div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>)}
+    </>
 
 
     )
