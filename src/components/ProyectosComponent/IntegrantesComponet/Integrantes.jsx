6e10e653ac62678eba9acdb1055516d5f122d59f
SoyDael <guhs202011004@tese.edu.mx>
2024-06-04 18:56:41 -0600

    Update Integrantes.jsx
    estilo solamente del boton

--- src/components/ProyectosComponent/IntegrantesComponet/Integrantes.jsx
+++ src/components/ProyectosComponent/IntegrantesComponet/Integrantes.jsx
@@ -99,7 +99,7 @@ const Integrantes = () => {
                                                     Asignar Actividad
                                                 </button>
                                             </td>
-                                            <td scope="row" className="px-10 py-4 font-medium text-indigo-700 dark:text-blue-500">
+                                            <td scope="row" className="px-6 py-4 font-medium text-gray-200 whitespace-nowrap dark:text text-transform: uppercase">
                                                 <button
                                                     type="button"
                                                     className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
